58db0a1985e7daa6fe31e53cb336f62f43cfe81f
Ilya Beda <ir4y.ix@gmail.com>
2025-01-31 15:54:18 +1100

    Change base url

--- contrib/emr-config/config.production.js
+++ contrib/emr-config/config.production.js
@@ -4,7 +4,7 @@ const config = {
     wearablesAccessConsentCodingSystem: 'https://fhir.emr.beda.software/CodeSystem/consent-subject',
 
     tier: 'production',
-    baseURL: 'https://aidbox.emr.beda.software',
+    baseURL: 'https://aidbox.fhirlab.net',
     sdcIdeUrl: 'https://sdc.beda.software',
     aiQuestionnaireBuilderUrl: 'https://builder.emr.beda.software',
 
